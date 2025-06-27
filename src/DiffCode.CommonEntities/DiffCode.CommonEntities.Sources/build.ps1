8f4ea4c82be03e6ca39122436c4d7df89d6918d4
Григорий Мелехов <gmelehov@live.ru>
2025-06-27 21:15:49 +0300

    Удален проект DiffCode.CommonEntities.Sources

--- src/DiffCode.CommonEntities/DiffCode.CommonEntities.Sources/build.ps1
+++
@@ -1,7 +0,0 @@
-$module = 'DiffCode.CommonEntities.Sources'
-$version = '1.0.4'
-
-
-dotnet restore
-dotnet pack -c Release -o bin\Release\publish
-dotnet nuget push bin\Release\publish\$module.$version.nupkg --source https://proget.diff.me:8625/nuget/NuGetPrivate/ --api-key $ENV:ProGetApiKey --skip-duplicate
